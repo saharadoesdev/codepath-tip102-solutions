f87cc7f2bd6db29e16b2848fcb7e90132947ae93
saharadoesdev <smsmo@att.net>
2025-07-24 20:06:03 -0500

    update in-class work for w8s2 standard problems

--- Week 08/Session 2 (08-02)/standard_problems.py
+++ Week 08/Session 2 (08-02)/standard_problems.py
@@ -109,45 +109,6 @@ print(count_odd_splits(monstera))
 print(count_odd_splits(None))
 
 
-from collections import deque
-
-class TreeNode():
-     def __init__(self, value, left=None, right=None):
-         self.val = value
-         self.left = left
-         self.right = right
-
-
-def build_tree(values):
-  if not values:
-      return None
-
-  def get_key_value(item):
-      if isinstance(item, tuple):
-          return item[0], item[1]
-      else:
-          return None, item
-
-  key, value = get_key_value(values[0])
-  root = TreeNode(value, key)
-  queue = deque([root])
-  index = 1
-
-  while queue:
-      node = queue.popleft()
-      if index < len(values) and values[index] is not None:
-          left_key, left_value = get_key_value(values[index])
-          node.left = TreeNode(left_value, left_key)
-          queue.append(node.left)
-      index += 1
-      if index < len(values) and values[index] is not None:
-          right_key, right_value = get_key_value(values[index])
-          node.right = TreeNode(right_value, right_key)
-          queue.append(node.right)
-      index += 1
-
-  return root
-
 
 def find_flower(inventory, name):
     if not inventory:
@@ -171,64 +132,6 @@ print(find_flower(garden, "Sunflower"))
 ## PROBLEM 3 was discussion-based, not coding!
 
 
-
-from collections import deque
-
-class TreeNode:
-  def __init__(self, value, key=None, left=None, right=None):
-      self.key = key
-      self.val = value
-      self.left = left
-      self.right = right
-
-
-def print_tree(root):
-    if not root:
-        return "Empty"
-    result = []
-    queue = deque([root])
-    while queue:
-        node = queue.popleft()
-        if node:
-            result.append(node.val)
-            queue.append(node.left)
-            queue.append(node.right)
-        else:
-            result.append(None)
-    while result and result[-1] is None:
-        result.pop()
-    print(result)
-
-
-def build_tree(values):
-  if not values:
-      return None
-
-  def get_key_value(item):
-      if isinstance(item, tuple):
-          return item[0], item[1]
-      else:
-          return None, item
-
-  key, value = get_key_value(values[0])
-  root = TreeNode(value, key)
-  queue = deque([root])
-  index = 1
-
-  while queue:
-      node = queue.popleft()
-      if index < len(values) and values[index] is not None:
-          left_key, left_value = get_key_value(values[index])
-          node.left = TreeNode(left_value, left_key)
-          queue.append(node.left)
-      index += 1
-      if index < len(values) and values[index] is not None:
-          right_key, right_value = get_key_value(values[index])
-          node.right = TreeNode(right_value, right_key)
-          queue.append(node.right)
-      index += 1
-
-  return root
 
 def add_plant(collection, name):
     if not collection:
